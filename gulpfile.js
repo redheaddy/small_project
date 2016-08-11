feadafe5a60d8d18387255e4d601e908de211830
Onokhova <onokhova@steelkiwi.com>
2016-08-11 12:45:06 +0300

    images added

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    minHtml = require('gulp-minify-html'),
+    imageMin = require('gulp-imagemin');
+    //pngMin = reqire('gulp-tinypng');
+
+gulp.task('minify-html', function() {
+  var opts = {
+    conditionals: true,
+    spare:true
+  };
+
+  return gulp.src('*.html')
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('/dist/'));
+});
